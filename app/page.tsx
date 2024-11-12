79fcc557aa26b9f2efcc946f0b6f4f8e8631470c
hhejo <litsmainjof@gmail.com>
2024-11-12 16:35:14 +0900

    feat: add count store

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
+'use client';
+import { useCountStore } from '@/store/store';
+
 export default function Home() {
+  const count = useCountStore((state) => state.count);
+  const increase = useCountStore((state) => state.increase);
+  const decrease = useCountStore((state) => state.decrease);
   return (
-    <>
-      <div>Welcome</div>
-    </>
+    <div className="flex flex-col items-center mt-4">
+      <h1 className="text-4xl font-bold">Welcome</h1>
+      <div className="text-xl font-bold">{count}</div>
+      <div>
+        <button className="text-2xl w-10 h-10 bg-sky-300" onClick={increase}>
+          +
+        </button>
+        <button className="text-2xl w-10 h-10 bg-red-300" onClick={decrease}>
+          -
+        </button>
+      </div>
+    </div>
   );
 }
